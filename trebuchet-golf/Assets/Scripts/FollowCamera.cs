52c336a13cc543562fd6530ea655464a815805d5
Daniel Dangond <dadangond@gmail.com>
2019-04-03 14:47:17 -0400

    Added camera following capabilities and support for different camera locations

--- trebuchet-golf/Assets/Scripts/FollowCamera.cs
+++ trebuchet-golf/Assets/Scripts/FollowCamera.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    public Transform target;
+    [Range(0,1)]
+    public float followSpeed = 0.5f;
+
+    private CameraLocation[] cameraLocations;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (target != null)
+        {
+            transform.LookAt(target);
+        }
+        cameraLocations = Object.FindObjectsOfType<CameraLocation>();
+        if (cameraLocations.Length == 0)
+        {
+            Debug.LogError("Please place CameraLocation objects in the scene!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateCameraPosition();
+        Quaternion currentRotation = transform.rotation;
+        transform.LookAt(target);
+        Quaternion targetRotation = transform.rotation;
+        transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, followSpeed);
+    }
+
+    void updateCameraPosition()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            MoveNearTarget();
+        }
+        if (cameraLocations.Length != 0)
+        {
+            Vector3 closestPosition = cameraLocations[0].transform.position;
+            float closestDistance = Vector3.Magnitude(closestPosition - target.position);
+            foreach (CameraLocation cl in cameraLocations)
+            {
+                float testDistance = Vector3.Magnitude(cl.transform.position - target.position);
+                if (testDistance < closestDistance)
+                {
+                    closestDistance = testDistance;
+                    closestPosition = cl.transform.position;
+                }
+            }
+            transform.position = closestPosition;
+        }
+    }
+
+    public void MoveNearTarget()
+    {
+        transform.position = target.position + new Vector3(1f,1f,1f);
     }
 }
