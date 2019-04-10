c67e083a69fd22bda27b4d9284c62e381c9fdfc5
ange <ange@mit.edu>
2019-04-10 00:59:12 -0400

    adding score + strt

---
+++ trebuchet-golf/Assets/Scripts/Navigator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    private bool loadingScene = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !loadingScene)
+            
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
+            Vector3 pos = Input.mousePosition;
+
+            Collider2D hitCollider = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(pos));
+
+            if (hitCollider) {
+                Debug.Log (hitCollider.gameObject.name);
+            }
+
+            if (hit.collider != null)
+            {
+                print((hit.collider.gameObject.name));
+                switch (hit.collider.gameObject.name)
+                {
+
+                    case "start":
+                        loadingScene = true;
+                        enabled = false;
+                        StartCoroutine(LoadScene("DanTest"));
+                        break;
+                    case "htp":
+                        loadingScene = true;
+                        enabled = false;
+                        StartCoroutine(LoadScene("howToPlay"));
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
+
---
+++ trebuchet-golf/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    public int numLaunches = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "# Launches: 0";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    
+    public void addLaunch()
+    {
+        numLaunches += 1;
+        UpdateScore();
+    }
+    
+    
+    void UpdateScore()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "# Launches: " + numLaunches;
+        }
+    }
+    
+}
