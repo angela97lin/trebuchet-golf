6bd79061bc317019aeb9252b58f9c6efd6a1fffa
Daniel Dangond <dadangond@gmail.com>
2019-04-17 15:08:57 -0400

    Fixed hole arrow being visible 180 degrees away from hole

--- trebuchet-golf/Assets/Scripts/Hole.cs
+++ trebuchet-golf/Assets/Scripts/Hole.cs
@@ -18,10 +18,11 @@ public class Hole : MonoBehaviour
 
     void OnDrawGizmos()
     {
+        BoxCollider col = GetComponent<BoxCollider>();
         Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x*5, transform.localScale.y, transform.localScale.z*5));
+        Gizmos.DrawWireCube(transform.position, new Vector3(col.size.x, col.size.y, col.size.z));
         Gizmos.color = Color.cyan;
-        Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x*5*.95f, transform.localScale.y*.95f, transform.localScale.z*5*.95f));
+        Gizmos.DrawWireCube(transform.position, new Vector3(col.size.x*.95f, col.size.y*.95f, col.size.z*.95f));
     }
 
     void OnTriggerEnter(Collider other)
--- trebuchet-golf/Assets/Scripts/HoleArrow.cs
+++ trebuchet-golf/Assets/Scripts/HoleArrow.cs
@@ -15,7 +15,16 @@ public class HoleArrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, holeArrowPosition.transform.position);
+        Vector3 holeDirection = holeArrowPosition.position - Camera.main.transform.position;
+        Vector3 cameraDirection = Camera.main.transform.forward;
+        if (Vector3.Dot(holeDirection, cameraDirection) > 0)
+        {
+            transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, holeArrowPosition.transform.position);
+        }
+        else
+        {
+            transform.position = new Vector3(100000, 100000, 0);
+        }
         transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 2, 0);
     }
 }
