82e487aeacb9588830817744fae2caf3f7ba58cf
Hayley Bergman <hayberg@mit.edu>
2019-04-17 14:22:21 -0400

    Added distance to hole

---
+++ trebuchet-golf/Assets/Scripts/DistanceToHole.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DistanceToHole : MonoBehaviour
+{
+
+    public Transform ball;
+    public Transform flag;
+
+    //public TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //scoreText.text = "Distance: ";
+    }
+
+    private Text textComponent;
+
+    private void Awake()
+    {
+        textComponent = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        string dist = "Distance: "+ Vector3.Distance(ball.position, flag.position).ToString("F1");
+        textComponent.text =  dist;
+    }
+}
