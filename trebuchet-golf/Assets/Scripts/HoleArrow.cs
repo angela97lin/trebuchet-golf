abf998964be625214f59a83c6f329f6b64580f3e
Daniel Dangond <dadangond@gmail.com>
2019-04-17 14:42:21 -0400

    Added arrow above hole

---
+++ trebuchet-golf/Assets/Scripts/HoleArrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleArrow : MonoBehaviour
+{
+    public Transform holeArrowPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, holeArrowPosition.transform.position);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 2, 0);
+    }
+}
