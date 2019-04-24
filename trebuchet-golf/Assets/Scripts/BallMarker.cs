d7458d128fb5342c03775f50c087cac9de05284a
jynnie <jynnie@mit.edu>
2019-04-24 15:53:04 -0400

    basic mini map

---
+++ trebuchet-golf/Assets/Scripts/BallMarker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMarker : MonoBehaviour
+{
+    GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.FindWithTag("Ball");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // transform.position.y = 241;
+        this.transform.position = new Vector3(ball.transform.position.x, this.transform.position.y, ball.transform.position.z);        
+    }
+}
